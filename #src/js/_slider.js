49be857962c6fc4a3e98f5449c2fcde9bf4eaef0
Vladislav <umorphine@gmail.com>
2021-03-31 18:35:04 +0400

    item.js && slider.js

---
+++ #src/js/_slider.js
@@ -0,0 +1,24 @@
+new Swiper('.same-vine__slider', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  slidesPerView: 1,
+  spaceBetween: 24,
+  breakpoints: {
+    320: {
+      slidesPerView: 1
+    },
+    720: {
+      slidesPerView: 2,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+      },
+    },
+    1200: {
+      slidesPerView: 4
+    }
+  }
+});
+
